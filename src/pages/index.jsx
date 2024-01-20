cd95e68b89ea94a6e119d45af7fb59a0899a04b1
YarinsHyper <yarin1857@gmail.com>
2024-01-20 23:09:46 +0200

    fix: education section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,7 @@ import { GitHubIcon, LinkedInIcon } from '@/components/SocialIcons'
 
 import amanLogo from '@/images/logos/aman-logo.png'
 import companyOneBiLogo from '@/images/logos/onebi-logo.png'
+import theOpenUniLogo from '@/images/logos/theOpenUni-logo.png'
 
 import { getAllExperiences } from '@/lib/getAllArticles'
 
@@ -76,7 +77,21 @@ function Resume() {
     }    
   ]
 
-  return (
+  let resume2 = [
+    {
+      company: 'The Open University',
+      title: 'B.Sc Computer Science',
+      logo: theOpenUniLogo,
+      start: '2024',
+      end: '2027'
+      // end: {
+      //   label: 'Present',
+      //   dateTime: new Date().getFullYear(),
+      // },
+    } 
+  ]
+
+  return (<div>
     <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <BriefcaseIcon className="h-6 w-6 flex-none" />
@@ -146,6 +161,69 @@ function Resume() {
         </svg>
         Download Resume
       </a> */}
+    </div>
+
+    <div className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <BriefcaseIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Education</span>
+      </h2>
+      <ol className="mt-6 space-y-4">
+        {resume2.map((role, roleIndex) => (
+          <li key={roleIndex} className="flex gap-4">
+            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
+            </div>
+            <dl className="flex flex-auto flex-wrap gap-x-2">
+              <dt className="sr-only">Company</dt>
+              <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                {role.company}
+              </dd>
+              <dt className="sr-only">Role</dt>
+              <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+                {role.title}
+              </dd>
+              <dt className="sr-only">Date</dt>
+              <dd
+                className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
+                aria-label={`${role.start.label ?? role.start} until ${
+                  role.end.label ?? role.end
+                }`}
+              >
+                <time dateTime={role.start.dateTime ?? role.start}>
+                  {role.start.label ?? role.start}
+                </time>{' '}
+                <span aria-hidden="true">—</span>{' '}
+                <time dateTime={role.end.dateTime ?? role.end}>
+                  {role.end.label ?? role.end}
+                </time>
+              </dd>
+            </dl>
+          </li>
+        ))}
+      </ol>
+      {/* <a
+        href="./Yarin-Benisty-Resume.pdf"
+        download="Yarin-Benisty-Resume.pdf"
+        class="inline-flex items-center rounded-lg bg-blue-500 px-4 py-2 font-semibold text-white transition duration-300 ease-in-out hover:bg-blue-600"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="mr-2 h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 17l-4 4m0 0l-4-4m4 4V3"
+          ></path>
+        </svg>
+        Download Resume
+      </a> */}
+    </div>
     </div>
   )
 }
