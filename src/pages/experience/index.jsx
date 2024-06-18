dcf5dd787b85be66ea8ebc7afabdf0aaff149f6d
YarinBen1 <yarin.b@onebi.co.il>
2024-06-18 20:05:10 +0300

    fix: another one

--- src/pages/experience/index.jsx
+++ src/pages/experience/index.jsx
@@ -45,7 +45,7 @@ export default function ExperienceIndex({ experiences }) {
       </Head>
       <SimpleLayout
         title="Experience"
-        intro="My personal past and present experience as a developer."
+        intro="My professional journey and current experience as a developer."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
