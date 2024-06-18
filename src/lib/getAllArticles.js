377ecb28ef69c47bf17477adc8c23bf9135ab4c5
YarinBen1 <yarin.b@onebi.co.il>
2024-06-18 19:48:24 +0300

    fix: latest + updated changes

--- src/lib/getAllArticles.js
+++ src/lib/getAllArticles.js
@@ -8,23 +8,33 @@ const experiences = [
     key: '1',
   },
   {
-    title: 'Drive',
+    title: 'Full Stack Developer',
     company: 'IDF Intelligence Unit',
     description:
-    'This system manages and stores classified military files like the known system "google drive". Actively contributed to the development of drive, Utilized Golang as the primary language along with technologies such as gRPC, Protobuf, vue.js, Docker, Kubernetes, Helm, azure, AWS, Express, TypeScript, Azure and MongoDB. Spearheaded transition to new technologies, including Node.js, React, Redux.js, Redis, RabbitMQ and AWS. The codebase followed the architecture style of micro-services and Agile methodology Recieved a certification of excellence for award winning system in the intelligence corps.',
-    date: '2021-04-01',
+      'Actively contributed to, mentored and led the transition to new technologies such as Golang, Node.js, React, Redux.js, Docker, Kubernetes, AWS, and MongoDB, adhering to micro-services architecture and Agile methodology. My expertise spans a range of languages and technologies including Vue.js, TypeScript, Redis, Azure and many more, with a track record of excellence recognized by a certification from the intelligence corps.',
+    date: '2022-10-13',
     key: '2',
   },
-  {
-    title: 'Transfers',
-    company: 'IDF Intelligence Unit',
-    description:
-    ' This system manages and transfers classified military files between calssified networks. Played a key leadership role in the development and implementation of the system. The codebase followed the architecture style of micro-services and Agile methodology and levraged Node.js as the primary language, along with a variety of technologies including React.js, Docker, Kubernetes, Redux, AWS, Express, TypeScript, MongoDB, and Redis.',
-    date: '2022-07-03',
-    key: '3',
-  },
+  // {
+  //   title: 'Drive',
+  //   company: 'IDF Intelligence Unit',
+  //   description:
+  //   'This system manages and stores classified military files like the known system "google drive". Actively contributed to the development of drive, Utilized Golang as the primary language along with technologies such as gRPC, Protobuf, vue.js, Docker, Kubernetes, Helm, azure, AWS, Express, TypeScript, Azure and MongoDB. Spearheaded transition to new technologies, including Node.js, React, Redux.js, Redis, RabbitMQ and AWS. The codebase followed the architecture style of micro-services and Agile methodology Recieved a certification of excellence for award winning system in the intelligence corps.',
+  //   date: '2021-04-01',
+  //   key: '3',
+  // },
+  // {
+  //   title: 'Transfers',
+  //   company: 'IDF Intelligence Unit',
+  //   description:
+  //   'This system manages and transfers classified military files between calssified networks. Played a key leadership role in the development and implementation of the system. The codebase followed the architecture style of micro-services and Agile methodology and levraged Node.js as the primary language, along with a variety of technologies including React.js, Docker, Kubernetes, Redux, AWS, Express, TypeScript, MongoDB, and Redis.',
+  //   date: '2022-07-03',
+  //   key: '4',
+  // },
 ]
 
 export async function getAllExperiences() {
-  return experiences.sort((a, z) => new Date(z.date) - new Date(a.date))
+  return experiences
+    .filter(experience => experience.date) // Filter out experiences without a date
+    .sort((a, z) => new Date(z.date) - new Date(a.date));
 }
